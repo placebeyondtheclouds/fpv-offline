321ca20b661b4450dfb1a58225ded0b16c7ebc68
Sasha <67932607+placebeyondtheclouds@users.noreply.github.com>
2026-05-18 08:57:34 +0800

    master

--- fw.sh
+++ fw.sh
@@ -2,10 +2,11 @@
 set -Eeuo pipefail
 
 : "${BETAFLIGHT_BRANCH:?BETAFLIGHT_BRANCH is required}"
-: "${BETAFLIGHT_VERSION:?BETAFLIGHT_VERSION is required}"
 : "${BETAFLIGHT_FW_TARGET:?BETAFLIGHT_FW_TARGET is required}"
 
 repo_dir="/opt/betaflight"
+release_name="${BETAFLIGHT_VERSION:-master}"
+source_ref="branch:$BETAFLIGHT_BRANCH"
 
 if [ ! -d "$repo_dir/.git" ]; then
   git clone https://github.com/betaflight/betaflight.git "$repo_dir"
@@ -14,8 +15,17 @@ else
   git -C "$repo_dir" fetch --tags --prune --force origin
 fi
 
-git -C "$repo_dir" fetch --tags --prune --force origin "$BETAFLIGHT_BRANCH"
-git -C "$repo_dir" checkout --detach "refs/tags/$BETAFLIGHT_VERSION"
+if [ "$BETAFLIGHT_BRANCH" = "master" ]; then
+  git -C "$repo_dir" fetch --prune --force origin master
+  git -C "$repo_dir" checkout -B master origin/master
+  release_name="master"
+else
+  : "${BETAFLIGHT_VERSION:?BETAFLIGHT_VERSION is required unless BETAFLIGHT_BRANCH=master}"
+  git -C "$repo_dir" fetch --tags --prune --force origin "$BETAFLIGHT_BRANCH"
+  git -C "$repo_dir" checkout --detach "refs/tags/$BETAFLIGHT_VERSION"
+  release_name="$BETAFLIGHT_VERSION"
+  source_ref="tag:$BETAFLIGHT_VERSION"
+fi
 
 cd "$repo_dir"
 
@@ -28,7 +38,7 @@ if ! make arm_sdk_install; then
   make arm_sdk_install
 fi
 
-make "$BETAFLIGHT_FW_TARGET" EXTRA_FLAGS="-D'RELEASE_NAME=$BETAFLIGHT_VERSION' ${BETAFLIGHT_FW_EXTRA_FLAGS:-}" -j
+make "$BETAFLIGHT_FW_TARGET" EXTRA_FLAGS="-D'RELEASE_NAME=$release_name' ${BETAFLIGHT_FW_EXTRA_FLAGS:-}" -j
 
 cp "$repo_dir"/obj/*.hex /fw/
 
@@ -37,6 +47,8 @@ printf 'HTTP_PROXY=%s\n' "${HTTP_PROXY:-}"
 printf 'HTTPS_PROXY=%s\n' "${HTTPS_PROXY:-}"
 printf 'ALL_PROXY=%s\n' "${ALL_PROXY:-}"
 printf 'BETAFLIGHT_BRANCH=%s\n' "$BETAFLIGHT_BRANCH"
-printf 'BETAFLIGHT_VERSION=%s\n' "$BETAFLIGHT_VERSION"
+printf 'BETAFLIGHT_SOURCE=%s\n' "$source_ref"
+printf 'BETAFLIGHT_VERSION=%s\n' "${BETAFLIGHT_VERSION:-ignored}"
+printf 'RELEASE_NAME=%s\n' "$release_name"
 printf 'BETAFLIGHT_FW_TARGET=%s\n' "$BETAFLIGHT_FW_TARGET"
 printf 'BETAFLIGHT_FW_EXTRA_FLAGS=%s\n' "${BETAFLIGHT_FW_EXTRA_FLAGS:-}"
